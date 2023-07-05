6d4a31021f232f19e586cb1867166042dec95521
MelowRainn <ruij0036@hz.nl>
2023-07-05 20:57:13 +0200

    help

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,40 @@
 <?php
 
-    use App\Http\Controllers\FooController;
-    use App\Http\Controllers\PostController;
-    use App\Models\Post;
-    use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-    /*
-    |--------------------------------------------------------------------------
-    | Web Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register web routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | contains the "web" middleware group. Now create something great!
-    |
-    */
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    // Take the 3 newest posts
+    $latestPosts = Post::orderBy('published_at', 'desc')->take(3)->get();
+    return view('welcome', compact('latestPosts'));
+})->name('home');
 
-    Route::get('/', function () {
-        // Take the 3 newest posts
-        $latestPosts = Post::orderBy('published_at', 'desc')->take(3)->get();
-        return view('welcome', compact('latestPosts'));
-    })->name('home');
+// Resource routes of the base pages.
+Route::resource('/posts', PostController::class);
 
-    // Resource routes of the base pages.
-    Route::resource('/posts', PostController::class);
+Route::resource('/foos', FooController::class);
 
-    Route::resource('/foos', FooController::class);
 
-    //    Route::get('/foos/edit', [FooController::class, 'edit']);
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
